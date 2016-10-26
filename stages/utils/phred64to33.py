bd1393390a98669a0c1cb3b6a592618f937104b8
Timothy J Becker <tbecker@mlg-jgm040.local.jax.org>
2016-10-26 13:55:08 -0400

    sync

--- stages/utils/phred64to33.py
+++ stages/utils/phred64to33.py
@@ -5,12 +5,12 @@ import sys
 
 for line in sys.stdin:
     if line.startswith('@'): #header
-        print(line)
+        print line,
     else:                    #alignments
         raw  = line.split('\t')
         base_qual = ''.join([chr(ord(i)-33) for i in raw[10]])
         if len(raw)>10:
-            print('\t'.join(raw[:10]+[base_qual]+raw[11:]))
+            print '\t'.join(raw[:10]+[base_qual]+raw[11:]),
         else:
-            print('\t'.join(raw[:10]+[base_qual]))
+            print '\t'.join(raw[:10]+[base_qual]),
             
