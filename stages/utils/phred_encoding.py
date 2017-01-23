f7ed808a4b7aaab504ccb43a8aa5cea5d61c3834
timothyjamesbecker <timothyjamesbecker@gmail.com>
2017-01-23 15:44:59 -0500

    phred encoding test

--- stages/utils/phred_encoding.py
+++ stages/utils/phred_encoding.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
-#guess the base quality encoding from a 1E6 randomly selected alignment records
-#used as: samtools view -Sh old.bam | SVE/stages/utils/phred_encoding.py 1E6 ./old.valid
 import sys
-import numpy as np
 
 args = sys.argv
-n     = int(round(float(args[1])))
+n,m     = int(round(float(args[1]))),0
+d = int(1E3)
 valid = args[2] 
 MIN,MAX = 255,0
 RANGES = {
@@ -16,22 +14,26 @@ RANGES = {
     'Illumina-1.5': (67, 104)
 }
 for line in sys.stdin:
-    if line.startswith('@') and np.random.choice([True,False],p=[1.0/n,1.0-1.0/n]): 
-        raw  = line.split('\t')
-        for i in raw[10]:
-            j = ord(i)
-            if j < MIN: MIN = j
-            if j > MAX: MAX = j
-if MIN <= 33: #sanger adds no new inputs for our validation pipeline
-    with open(valid,'a') as f:     f.write('\nbase quality encoding = Sanger:33-73\n')
-    sys.exit()
-elif MIN > 33 and MAX > 73:
-    if MIN < 64:
-        with open(valid,'a') as f: f.write('\nbase quality encoding = Solexa:59-104\n')
+    if not line.startswith('@'):
+        m += 1
+        if m%d==0: print('MAX=%s\tMIN=%s'%(MAX,MIN))
+        raw  = [ord(j) for j in line.split('\t')[10]]
+        for r in raw:
+            if MIN > r: MIN = r
+            if MAX < r: MAX = r
+    if MIN <= 33: #sanger adds no new inputs for our validation pipeline
+        with open(valid,'a') as f:     f.write('base quality encoding = Sanger:33-73\n')
         sys.exit()
-    elif MIN < 67:
-        with open(valid,'a') as f: f.write('\nbase quality encoding = Illumina-1.3:64-104\n')
-        sys.exit()
-    else:
-        with open(valid,'a') as f: f.write('\nbase quality encoding = Illumina-1.5:67-104\n')
-        sys.exit()
+    elif MIN > 33 and MAX > 73:
+        if MIN < 64:
+            with open(valid,'a') as f: f.write('base quality encoding = Solexa:59-104\n')
+            sys.exit()
+        elif MIN < 67:
+            with open(valid,'a') as f: f.write('base quality encoding = Illumina-1.3:34-104\n')
+            sys.exit()
+        else:
+            with open(valid,'a') as f: f.write('base quality encoding = Illumina-1.5:67-104\n')
+            sys.exit()
+    if m>n: break
+with open(valid,'a') as f:          f.write('base quality encoding = Unkown Illumina:34-104\n')
+sys.exit()
