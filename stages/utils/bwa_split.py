7cdb28996a0b44c43778218520f45532d1801905
timothyjamesbecker <timothyjamesbecker@gmail.com>
2017-02-19 16:18:59 -0500

    fastq interleved anonymous pipe test

--- stages/utils/bwa_split.py
+++ stages/utils/bwa_split.py
@@ -2,12 +2,10 @@
 import argparse
 import os
 import time
-import itertools as it
 import subprocess32 as subprocess
 import multiprocessing as mp
-import pysam
 
-parser = argparse.ArgumentParser(description='SVE:FASTQ splitting,alignment,mapping test pipeline V-4.0')
+parser = argparse.ArgumentParser(description='SVE:|| FASTQ splitting,alignment,mapping test pipeline 0.4.1')
 parser.add_argument('-r', '--ref_path',type=str, help='reference fasta file to align to\t[None]')
 parser.add_argument('-f', '--fastq',type=str, help='single fastq or comma separated\t[None]')
 parser.add_argument('-o', '--out_dir',type=str, help='output directory\t[same as input on fastq]')
@@ -21,7 +19,7 @@ split       = args.split_factor
 if os.path.exists(out_dir): subprocess.call(['rm','-rf',out_dir])
 os.makedirs(out_dir)
 software = os.path.dirname(os.path.abspath(__file__))+'/../../../' #/software/SVE/stages/utils/../../../
-print(software)#-----------------------------------------------
+router        = software+'/SVE/stages/utils/fastq_router.py'
 bwa_mem       = software+'/bwa-master/bwa mem' 
 samtools_view = software+'/samtools-1.3/samtools view'
 #--------------------------------------------------------------
@@ -29,68 +27,19 @@ samtools_view = software+'/samtools-1.3/samtools view'
 result_list = []
 def collect_results(result):
     result_list.append(result)
-    
-#we are assuming that there is a pair of fastq files here for bwa mem input
-#make 2xsplit number of output pipes avaible to the system
-def make_pipes(fq,split,out_dir):
-    P,base_name = {},fq.rsplit('/')[-1].rsplit('.')[0].rsplit('_')[0]
-    for i in range(split):
-        for j in range(2):
-            if P.has_key(i):
-                P[i][j] = out_dir+'/'+base_name+'.pipe.%s.'%j+str(i)
-            else:
-                P[i] = {j:out_dir+'/'+base_name+'.pipe.%s.'%j+str(i)}
-    for i in range(split):
-        for j in range(2):
-            if os.path.exists(P[i][j]): os.remove(P[i][j])
-            os.mkfifo(P[i][j])
-    return P
-
-#do one pipe at a time, but can open multiple files
-#file reading on f, pipe writting on p s is the modulatoing offset
-def pipe_writer(in_f,out_p,s):
-    x,lines = 0,[]
-    with pysam.FastxFile(in_f,'rb') as f: #can read compressed inputs
-        print('opening: in_f=%s\tout_p=%s\ts=%s'%(in_f,out_p,s))
-        with open(out_p,'w',0) as p:
-            print('%s past blocking W call'%out_p)
-            for entry in f: #[1] get the fastq records
-                if (x+s)%4==0: #this is the jump index for number of fastq rows
-                    line = '\n'.join(['+' if v is None else v for v in \
-                             [entry.name,entry.sequence,
-                              entry.comment,entry.quality]])+'\n'
-                    p.write(line)
-                    lines += [line]
-                x += 1
-    return lines
-
-def pipe_reader(P,i,ref_path):
-    out,output = P[i][0].rsplit('.pipe.')[0]+'.%s.fq'%i,None
-    with open(P[i][0],'r') as p1:
-        print('%s past blocking R call'%P[i][0])
-        with open(P[i][1],'r') as p2:
-            print('%s past blocking R call'%P[i][1])
-            with open(out,'w') as f:
-                x,values = 0,{0:[],1:[]}
-                for lines in it.izip(*[p1,p2]):
-                    print lines[0],
-                    print lines[1],
-                    values[0] += [lines[0]]
-                    values[1] += [lines[1]]
-                    if x%4==3:
-                        f.write(''.join(values[0]+values[1]))
-                        values[0],values[1] = [],[]
-                    x += 1
-    return ['finished writing and %s have been released'%P[i]]
                 
-#No try to get bwa mem working on these pipes...
-def fastq_bwa_pipe_reader(P,i,ref_path):
-    out,output = P[i][0].rsplit('.pipe.')[0]+'.%s.bam'%i,None #paired coding
-    piped_mem = [bwa_mem,'-M',ref_path,'<(cat %s)'%P[i][0],'<(cat %s)'%P[i][1],'|',
-                 samtools_view,'-Sb','-','>',out]
-    try:#bwa mem cll here-------------------------------------------
+#one of the split values to assemble...
+def fastq_bwa_mem_piped(fastqs,i,out_dir,ref_path):
+    output = ''
+    bam = out_dir+'/'+fastqs[0].rsplit('/')[-1].rsplit('.fq')[0]+'.%s.bam'%i
+    piped_mem = [router,'-f',','.join(fastqs),'-s','|',
+                 bwa_mem,'-M','-p',ref_path,'|',   #-t for threads
+                 samtools_view,'-Sb','-','>',bam]  #-@ for threads here
+    try:#bwa mem call here-------------------------------------------
         output += subprocess.check_output(' '.join(piped_mem),
-                                          shell=True,executable='/bin/bash')
+                                          stderr=subprocess.STDOUT,
+                                          executable='/bin/bash',
+                                          shell=True)
     except Exception as E:
         output += str(E) #error on the call-------------------------
     return [output]
@@ -98,33 +47,15 @@ def fastq_bwa_pipe_reader(P,i,ref_path):
 #|| by number of fastq files presented
 if __name__ == '__main__':
     print('found %s fastq files'%len(fastq_files))
-    F = make_pipes(fastq_files[0],split,out_dir)
-    
-    p = mp.Pool(processes=3)
-    print('starting process forking and IPC')
-    for i in F:
-        #now lets try one reader for every two write pipes:
-        for j in F[i]: #fq1_pipe + fq2_pipe => paired_fastq_pipe_reader
-            print('dispatching pipe writer %s'%F[i][j])
-            p.apply_async(pipe_writer,
-                          args=(fastq_files[j],F[i][j],i),
-                          callback=collect_results)
-            time.sleep(0.25)
-        print('dispatching pipe reader %s'%F[i])
-        p.apply_async(fastq_bwa_pipe_reader,
-                       args=(F,i,ref_path),
+    p = mp.Pool(processes=split)
+    for i in range(split):
+        print('dispatching bwa mem process %s'%i)
+        p.apply_async(fastq_bwa_mem_piped,
+                       args=(fastq_files,i,out_dir,ref_path),
                        callback=collect_results)
     p.close()
     p.join()
     print('all processes have been joined!!!')
-    for i in result_list:
-        print(i)
-    
-    ready = []
-    for i in F:
-        for j in F[i]:
-            os.remove(F[i][j])
-            ready += [os.path.exists(F[i][j])]
-    print('named pipes deleted = %s'%(not any(ready)))
+    for i in result_list: print(i)
 #---------------------------------------------------------------------------------------------------------------
 #/software/SVE/stages/utils/bwa_split.py -r /data/human_g1k_v37_decoy/human_g1k_v37_decoy.fa -f /data/fastq/test_1.fq.gz,/data/fastq/test_2.fq.gz -o /data/test/ -s 4    
---
+++ stages/utils/fastq_router.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import os
+import itertools as it
+import pysam
+des = """
+SVE:FASTQ PE routing, splitting and interleveing tool\n
+fastq_router.py -f /data/fastq/test_1.fq.gz,/data/fastq/test_2.fq.gz -s 4 | bwa mem -M -p...
+"""
+parser = argparse.ArgumentParser(description=des)
+parser.add_argument('-f', '--fastq',type=str, help='single fastq or comma separated\t[None]')
+parser.add_argument('-s', '--split_index',type=int, help='splitting index\t[4]')
+args = parser.parse_args()
+fastqs,split = args.fastq.split(','),args.split_factor
+if not all([os.path.exists(f) for f in fastqs]): raise IOError
+#main iterator reads in two file handles and outputs interleved
+with pysam.FastxFile(fastqs[0],'rb') as f1:
+    with pysam.FastxFile(fastqs[1],'rb') as f2:
+        x,values = 0, []
+        for reads in it.izp(*[f1,f2]):
+            if (x+split)%4==0: #default 4 lines of fastq format
+                for i in range(2):
+                    values += [reads[i].name,reads[i].sequence,
+                               reads[i].comment,reads[i].quality]
+                values = ['+' if v is None else v for v in values]
+                print('\n'.join(values)+'\n') #could do name check assertion here
+                values = []
+            x += 1
+sys.exit()
