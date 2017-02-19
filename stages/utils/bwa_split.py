3fbd136194e0da568f759e44e2cbdd1d6830a632
timothyjamesbecker <timothyjamesbecker@gmail.com>
2017-02-19 12:30:29 -0500

    name piped debuging

--- stages/utils/bwa_split.py
+++ stages/utils/bwa_split.py
@@ -18,7 +18,8 @@ ref_path    = args.ref_path
 fastq_files = args.fastq.split(',')
 out_dir     = args.out_dir
 split       = args.split_factor
-if not os.path.exists(out_dir): os.makedirs(out_dir)
+if os.path.exists(out_dir): subprocess.call(['rm','-rf',out_dir])
+os.makedirs(out_dir)
 software = os.path.dirname(os.path.abspath(__file__))+'/../../../' #/software/SVE/stages/utils/../../../
 print(software)#-----------------------------------------------
 bwa_mem       = software+'/bwa-master/bwa mem' 
@@ -48,50 +49,51 @@ def make_pipes(fq,split,out_dir):
 #do one pipe at a time, but can open multiple files
 #file reading on f, pipe writting on p s is the modulatoing offset
 def pipe_writer(in_f,out_p,s):
-    x = 0
+    x,types = 0,[]
     with pysam.FastxFile(in_f,'rb') as f: #can read compressed inputs
         print('opening: in_f=%s\tout_p=%s\ts=%s'%(in_f,out_p,s))
         with open(out_p,'w',0) as p:
             print('%s past blocking W call'%out_p)
+            p.write('\n')
             for entry in f: #[1] get the fastq records
                 if (x+s)%4==0: #this is the jump index for number of fastq rows
                     line = '\n'.join(['+' if v is None else v for v in \
                              [entry.name,entry.sequence,
                               entry.comment,entry.quality]])+'\n'
+                    types += [type(t) for t in [entry.name,entry.sequence,entry.comment,entry.quality]]
                     p.write(line)
                 x += 1
-    return 'wrote all data into pipe %s'%out_p
+    return types
 
 def pipe_reader(P,i,ref_path):
     out,output = P[i][0].rsplit('.pipe.')[0]+'.%s.fq'%i,None
-    with open(P[i][0],'w') as p1:
+    with open(P[i][0],'r') as p1:
         print('%s past blocking R call'%P[i][0])
-        with open(P[i][1],'w') as p2:
+        with open(P[i][1],'r') as p2:
             print('%s past blocking R call'%P[i][1])
             with open(out,'w') as f:
                 x,values = 0,{0:[],1:[]}
                 for lines in it.izip(*[p1,p2]):
                     print lines[0],
                     print lines[1],
+                    values[0] += [lines[0]]
+                    values[1] += [lines[1]]
                     if x%4==3:
-                        values[0] += [lines[0]]
-                        values[1] += [lines[1]]
                         f.write(''.join(values[0]+values[1]))
                         values[0],values[1] = [],[]
+                    x += 1
     return 'finished writing and %s have been released'%P[i]
                 
 #No try to get bwa mem working on these pipes...
-def paired_fastq_pipe_bwa_reader(P,i,ref_path):
+def fastq_bwa_pipe_reader(P,i,ref_path):
     out,output = P[i][0].rsplit('.pipe.')[0]+'.%s.bam'%i,None #paired coding
-    piped_mem = [bwa_mem,'-M',ref_path,'<(cat %s)'%P[i][0],'<(cat %s)'%P[i][1],'>',out]#,'|',
-                #samtools_view,'-Sb','-','>',out]
+    piped_mem = [bwa_mem,'-M',ref_path,'<(cat %s)'%P[i][0],'<(cat %s)'%P[i][1],'|',
+                 samtools_view,'-Sb','-','>',out]
     try:#bwa mem cll here-------------------------------------------
-        output = subprocess.Popen(' '.join(piped_mem),
-                                   shell=True,
-                                   executable='/bin/bash')
-        output.wait()
+        output += subprocess.check_output(' '.join(piped_mem),
+                                          shell=True,executable='/bin/bash')
     except Exception as E:
-        output = str(E) #error on the cal-------------------------
+        output += str(E) #error on the call-------------------------
     return output
     
 #|| by number of fastq files presented
@@ -102,11 +104,6 @@ if __name__ == '__main__':
     p = mp.Pool(processes=3*split)
     print('starting process forking and IPC')
     for i in F:
-#        print('dispatching pipe reader %s'%F[i])
-#        p.apply_async(paired_fastq_pipe_bwa_reader,
-#                       args=(F,i,ref_path),
-#                       callback=collect_results)
-#        time.sleep(1)
         #now lets try one reader for every two write pipes:
         for j in F[i]: #fq1_pipe + fq2_pipe => paired_fastq_pipe_reader
             print('dispatching pipe writer %s'%F[i][j])
@@ -114,10 +111,17 @@ if __name__ == '__main__':
                           args=(fastq_files[j],F[i][j],i),
                           callback=collect_results)
             time.sleep(0.25)
+    time.sleep(0.5)
+    for i in F:
+        print('dispatching pipe reader %s'%F[i])
+        p.apply_async(fastq_bwa_pipe_reader,
+                       args=(F,i,ref_path),
+                       callback=collect_results)
     p.close()
     p.join()
     print('all processes have been joined!!!')
-    for result in result_list: print(result)
+    for i in result_list:
+        print(i)
     
     ready = []
     for i in F:
@@ -125,4 +129,5 @@ if __name__ == '__main__':
             os.remove(F[i][j])
             ready += [os.path.exists(F[i][j])]
     print('named pipes deleted = %s'%(not any(ready)))
-#---------------------------------------------------------------------------------------------------------------    
+#---------------------------------------------------------------------------------------------------------------
+#/software/SVE/stages/utils/bwa_split.py -r /data/human_g1k_v37_decoy/human_g1k_v37_decoy.fa -f /data/fastq/test_1.fq.gz,/data/fastq/test_2.fq.gz -o /data/test/ -s 4    
