dc667e546029556cbe2000f06d5d82553e8988ec
Wan-ping Lee <leew@helix.(none)>
2017-03-10 13:50:09 -0500

    For bwa mem algorithm, adding commands to convert sam to bam.

--- scripts/prepare_bam.py
+++ scripts/prepare_bam.py
@@ -176,7 +176,12 @@ with svedb.SVEDB(dbc['srv'], dbc['db'], dbc['uid'], dbc['pwd']) as dbo:
                 outs = st.run(run_id,{'.fa':[ref_fa_path],'.fq':reads,
                                       'platform_id':['illumina'],
                                       'SM':[SM],
-                                      'out_dir':[directory]}) 
+                                      'out_dir':[directory]})
+ 
+            	#picard conversion and RG additions needed for gatk VC...
+            	st = stage.Stage('picard_sam_convert',dbc)
+            	outs = st.run(run_id,{'.sam':[l+ids['bwa_sampe']+'.sam']})
+
             if not os.path.exists(directory+base+'.bam'): #look for the finished sorted bam file
                 #picard sam to bam, sort, mark duplicates and index
                 st = stage.Stage('picard_sam_convert',dbc)
