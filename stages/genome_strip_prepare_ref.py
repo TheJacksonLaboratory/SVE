0df2c244f65836adf27da6c543d7080187114ba0
Wan-ping Lee <wanping.lee@gmail.com>
2017-05-09 16:00:05 -0400

    Make GenomeSTRiP work.

--- stages/genome_strip_prepare_ref.py
+++ stages/genome_strip_prepare_ref.py
@@ -64,41 +64,36 @@ class genome_strip(stage_wrapper.Stage_Wrapper):
             print "ERROR: .fa is required for genome_strip_prepare_ref.py"
             return None
 
-        fa_path = self.strip_name(inputs['.fa'])
-        
         #will have to figure out output file name handling
+        # Since GenomeSTRiP uses old bwa to index the reference, so we make the other copy.
         gs_ref = self.strip_in_ext(inputs['.fa'],'.fa') + '_' + str(self.stage_id) + '.fa'
         cascade = self.strip_in_ext(gs_ref,'.fa')
-        out_names = {'.dict': cascade + '.dict',
-                     '.ploidymap.txt':cascade + '.ploidymap.txt',
-                     '.rdmask.bed':cascade + '.rdmask.bed',
-                     '.svmask.fasta':cascade + '.svmask.fasta',
-                     '.gendermask.bed':cascade + '.gendermask.bed',
-                     '.gcmask.fasta':cascade + '.gc2mask.fasta'}
+        out_names = {'.svmask.fasta':  None,
+                     '.ploidymap.txt': None,
+                     '.rdmask.bed':    None,
+                     '.gendermask.bed':None,
+                     '.gcmask.fasta':  None,
+                     'bundle_dir':     None}
 
         # Download genome_strip bundle if there is
         if 'genome' in inputs:
-            bundle_link, bundle_file, bundle_dir = '', '', ''
+            bundle_link, bundle_file = '', ''
             if inputs['genome'] == 'hg19':
                 bundle_link = "ftp://ftp.broadinstitute.org/pub/svtoolkit/reference_metadata_bundles/Homo_sapiens_assembly19_12May2015.tar.gz"
                 bundle_file = self.tools['SVE_HOME'] + '/data/gs_hg19_bundle_file.tar.gz'
-                bundle_dir  = self.tools['SVE_HOME'] + '/data/Homo_sapiens_assembly19'
-                out_names['.ploidymap.txt'] = bundle_dir + '/Homo_sapiens_assembly19.ploidymap.txt'
-                out_names['.rdmask.bed'] = bundle_dir + '/Homo_sapiens_assembly19.rdmask.bed'
-                out_names['.svmask.fasta'] = bundle_dir + '/Homo_sapiens_assembly19.svmask.fasta'
-                out_names['.gendermask.bed'] = bundle_dir + '/Homo_sapiens_assembly19.gendermask.bed'
-                out_names['.gcmask.fasta'] = bundle_dir + '/Homo_sapiens_assembly19.gcmask.fasta'
+                out_names['bundle_dir'] = self.tools['SVE_HOME'] + '/data/Homo_sapiens_assembly19'
+                gs_ref = out_names['bundle_dir'] + '/Homo_sapiens_assembly19.fasta'
+                out_names['.dict'] = self.strip_in_ext(gs_ref,'.fa') + '.dict'
+                out_names['.svmask.fasta'] = out_names['bundle_dir'] + '/Homo_sapiens_assembly19.svmask.fasta'
             elif inputs['genome'] == 'hg38':
                 bundle_link = "ftp://ftp.broadinstitute.org/pub/svtoolkit/reference_metadata_bundles/Homo_sapiens_assembly38_12Oct2016.tar.gz"
                 bundle_file = self.tools['SVE_HOME'] + '/data/gs_hg38_bundle_file.tar.gz'
-                bundle_dir  = self.tools['SVE_HOME'] + '/data/Homo_sapiens_assembly38'
-                out_names['.ploidymap.txt'] = bundle_dir + '/Homo_sapiens_assembly38.ploidymap.txt'
-                out_names['.rdmask.bed'] = bundle_dir + '/Homo_sapiens_assembly38.rdmask.bed'
-                out_names['.svmask.fasta'] = bundle_dir + '/Homo_sapiens_assembly38.svmask.fasta'
-                out_names['.gendermask.bed'] = bundle_dir + '/Homo_sapiens_assembly38.gendermask.bed'
-                out_names['.gcmask.fasta'] = bundle_dir + '/Homo_sapiens_assembly38.gcmask.fasta'
+                out_names['bundle_dir'] = self.tools['SVE_HOME'] + '/data/Homo_sapiens_assembly38'
+                gs_ref = out_names['bundle_dir'] + '/Homo_sapiens_assembly38.fasta'
+                out_names['.dict'] = self.strip_in_ext(gs_ref,'.fa') + '.dict'
+                out_names['.svmask.fasta'] = out_names['bundle_dir'] + '/Homo_sapiens_assembly38.svmask.fasta'
 
-            if not os.path.exists(bundle_dir):
+            if out_names['bundle_dir'] != None and not os.path.exists(out_names['bundle_dir']):
                 if not os.path.isfile(bundle_file):
                     print ("<<<<<<<<<<<<<SVE command>>>>>>>>>>>>>>>\n")
                     download = ['wget', '-O', bundle_file, bundle_link]
@@ -115,86 +110,61 @@ class genome_strip(stage_wrapper.Stage_Wrapper):
         classpath = SV_DIR+'/lib/SVToolkit.jar:'+SV_DIR+'/lib/gatk/GenomeAnalysisTK.jar:'+SV_DIR+'/lib/gatk/Queue.jar'
         cgm  = 'org.broadinstitute.sv.apps.ComputeGenomeMask'
 
-        #[3] gender_map this is for each sample...       
-        
-        #self.db_start(run_id,out_names['.fa'])        
         #[3a]execute the command here----------------------------------------------------
-        output,err = '',{}
-        try:
-            if not os.path.isfile(gs_ref):
-                copy = ['cp', inputs['.fa'], gs_ref]
-                output += subprocess.check_output(' '.join(copy),stderr=subprocess.STDOUT,shell=True)
-            if not all([os.path.isfile(gs_ref+'.'+suffix) for suffix in ['amb','ann','bwt','pac','sa','rbwt','rpac','rsa']]):
-                indexref  = [self.tools['GENOME_STRIP_PATH'] + '/bwa/bwa', 'index', gs_ref]
-                print ("<<<<<<<<<<<<<SVE command>>>>>>>>>>>>>>>\n")
-                print (' '.join(indexref))
-                output += subprocess.check_output(' '.join(indexref),stderr=subprocess.STDOUT,shell=True)
+        if not os.path.isfile(gs_ref):
+            copy = ['cp', inputs['.fa'], gs_ref]
+            subprocess.check_output(' '.join(copy),stderr=subprocess.STDOUT,shell=True)
 
-            if not os.path.isfile(gs_ref+'.fai'):
-                faidx     = [self.tools['SAMTOOLS'],'faidx',gs_ref]
-                print ("<<<<<<<<<<<<<SVE command>>>>>>>>>>>>>>>\n")
-                print (' '.join(faidx))
-                output += subprocess.check_output(' '.join(faidx),stderr=subprocess.STDOUT,shell=True)
+        if not all([os.path.isfile(gs_ref+'.'+suffix) for suffix in ['amb','ann','bwt','pac','sa','rbwt','rpac','rsa']]):
+            indexref  = [self.tools['GENOME_STRIP_PATH'] + '/bwa/bwa', 'index', gs_ref]
+            print ("<<<<<<<<<<<<<SVE command>>>>>>>>>>>>>>>\n")
+            print (' '.join(indexref))
+            subprocess.check_output(' '.join(indexref),stderr=subprocess.STDOUT,shell=True)
 
-            if not os.path.isfile(out_names['.dict']):
-                dictbuild = [self.tools['JAVA-1.8'], '-jar', self.tools['PICARD'], 'CreateSequenceDictionary', 'R='+gs_ref, 'O='+out_names['.dict']]
-                print ("<<<<<<<<<<<<<SVE command>>>>>>>>>>>>>>>\n")
-                print (' '.join(dictbuild))
-                output += subprocess.check_output(' '.join(dictbuild),stderr=subprocess.STDOUT,shell=True)
+        if not os.path.isfile(gs_ref+'.fai'):
+            faidx     = [self.tools['SAMTOOLS'],'faidx',gs_ref]
+            print ("<<<<<<<<<<<<<SVE command>>>>>>>>>>>>>>>\n")
+            print (' '.join(faidx))
+            subprocess.check_output(' '.join(faidx),stderr=subprocess.STDOUT,shell=True)
 
-            if not os.path.isfile(out_names['.svmask.fasta']):
-                LD_LIB = self.tools['GENOME_STRIP_PATH']+'/bwa'
-                if os.environ.has_key('LD_LIBRARY_PATH'): LD_LIB += ':'+os.environ['LD_LIBRARY_PATH']
-                svmask    = [self.tools['JAVA-1.8'], '-cp', classpath, cgm, '-R', gs_ref, '-O', out_names['.svmask.fasta'], '-readLength',str(100)]
-                print ("<<<<<<<<<<<<<SVE command>>>>>>>>>>>>>>>\n")
-                print (' '.join(svmask))
-                output += subprocess.check_output(' '.join(svmask),stderr=subprocess.STDOUT,shell=True,
-                                                 env={'LD_LIBRARY_PATH':LD_LIB})
+        if not os.path.isfile(out_names['.dict']):
+            dictbuild = [self.tools['JAVA-1.8'], '-jar', self.tools['PICARD'], 'CreateSequenceDictionary', 'R='+gs_ref, 'O='+out_names['.dict']]
+            print ("<<<<<<<<<<<<<SVE command>>>>>>>>>>>>>>>\n")
+            print (' '.join(dictbuild))
+            subprocess.check_output(' '.join(dictbuild),stderr=subprocess.STDOUT,shell=True)
 
-            if not os.path.isfile(out_names['.ploidymap.txt']) or not os.path.isfile(out_names['.rdmask.bed']):
-                seq_n = sr.get_fasta_seq_names(gs_ref) #assume last two are sex chroms
-                seq_l = sr.get_fasta_seq_lens(gs_ref)  #get the lens here
-                if not os.path.isfile(out_names['.ploidymap.txt']): generate_ploidy(seq_n, seq_l, out_names['.ploidymap.txt'])
-                if not os.path.isfile(out_names['.rdmask.bed']): generate_rdmask(seq_n, seq_l, out_names['.rdmask.bed'])
+        if not os.path.isfile(out_names['.svmask.fasta']):
+            LD_LIB = self.tools['GENOME_STRIP_PATH']+'/bwa'
+            if os.environ.has_key('LD_LIBRARY_PATH'): LD_LIB += ':'+os.environ['LD_LIBRARY_PATH']
+            svmask    = [self.tools['JAVA-1.8'], '-cp', classpath, cgm, '-R', gs_ref, '-O', out_names['.svmask.fasta'], '-readLength',str(100)]
+            print ("<<<<<<<<<<<<<SVE command>>>>>>>>>>>>>>>\n")
+            print (' '.join(svmask))
+            subprocess.check_output(' '.join(svmask),stderr=subprocess.STDOUT,shell=True,
+                                    env={'LD_LIBRARY_PATH':LD_LIB})
+
+        if ((out_names['.ploidymap.txt'] != None and not os.path.isfile(out_names['.ploidymap.txt'])) 
+            or (out_names['.rdmask.bed'] != None and not os.path.isfile(out_names['.rdmask.bed']))):
+            seq_n = sr.get_fasta_seq_names(gs_ref) #assume last two are sex chroms
+            seq_l = sr.get_fasta_seq_lens(gs_ref)  #get the lens here
+            if not os.path.isfile(out_names['.ploidymap.txt']): generate_ploidy(seq_n, seq_l, out_names['.ploidymap.txt'])
+            if not os.path.isfile(out_names['.rdmask.bed']): generate_rdmask(seq_n, seq_l, out_names['.rdmask.bed'])
                  
-            
-        #catch all errors that arise under normal call behavior
-        except subprocess.CalledProcessError as E:
-            print('call error: '+E.output)        #what you would see in the term
-            err['output'] = E.output
-            #the python exception issues (shouldn't have any...
-            print('message: '+E.message)          #?? empty
-            err['message'] = E.message
-            #return codes used for failure....
-            print('code: '+str(E.returncode))     #return 1 for a fail in art?
-            err['code'] = E.returncode
-        except OSError as E:
-            print('os error: '+E.strerror)        #what you would see in the term
-            err['output'] = E.strerror
-            #the python exception issues (shouldn't have any...
-            print('message: '+E.message)          #?? empty
-            err['message'] = E.message
-            #the error num
-            print('code: '+str(E.errno))
-            err['code'] = E.errno
-        print('output:\n'+output)
-       
         #[3b]check results--------------------------------------------------
-        if err == {}:
-            #self.db_stop(run_id,{'output':output},'',True)
-            results = {'.fa':gs_ref,
-                       '.svmask.fasta':  out_names['.svmask.fasta'],
-                       '.ploidymap.txt': out_names['.ploidymap.txt'],
-                       '.rdmask.bed':    out_names['.rdmask.bed'],
-                       '.gendermask.bed':out_names['.gendermask.bed'],
-                       '.gcmask.fasta':  out_names['.gcmask.fasta']}
-            #for i in results: print i
-            if all([os.path.isfile(r) for key,r in results.iteritems()]):
-                print("<<<<<<<<<<<<<genome_strip_prepare_ref sucessfull>>>>>>>>>>>>>>>\n")
-                return results   #return a list of names
-            else:
-                print("<<<<<<<<<<<<<genome_strip_prepare_ref failure>>>>>>>>>>>>>>>\n")
-                return None
+        #self.db_stop(run_id,{'output':output},'',True)
+        results = {'.fa':gs_ref,
+                   '.svmask.fasta':  out_names['.svmask.fasta'],
+                   '.ploidymap.txt': out_names['.ploidymap.txt'],
+                   '.rdmask.bed':    out_names['.rdmask.bed'],
+                   '.gendermask.bed':out_names['.gendermask.bed'],
+                   '.gcmask.fasta':  out_names['.gcmask.fasta']}
+        #for i in results: print i
+        if os.path.isfile(results['.fa']) and out_names['bundle_dir'] != None and os.path.exists(out_names['bundle_dir']):
+            print("<<<<<<<<<<<<<genome_strip_prepare_ref sucessfull>>>>>>>>>>>>>>>\n")
+            results['bundle_dir'] = out_names['bundle_dir']
+            return results   #return a list of names
+        elif all([os.path.isfile(r) for key,r in results.iteritems()]):
+            print("<<<<<<<<<<<<<genome_strip_prepare_ref sucessfull>>>>>>>>>>>>>>>\n")
+            return results   #return a list of names
         else:
-            #self.db_stop(run_id,{'output':output},err['message'],False)
+            print("<<<<<<<<<<<<<genome_strip_prepare_ref failure>>>>>>>>>>>>>>>\n")
             return None
