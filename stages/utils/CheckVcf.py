2d4a3ae0e82d8b5732265ddbc89afafaecb11930
Wan-ping Lee <wanping.lee@gmail.com>
2017-08-03 19:30:12 -0400

    new file:   stages/utils/CheckVcf.py

---
+++ stages/utils/CheckVcf.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+def GetCallCount(vcf_name):
+    if not os.path.isfile(vcf_name): return 0
+    with open(vcf_name) as file:
+        content = file.readlines()
+    header = [x for x in content if x[0] == "#"]
+    return len(content) - len(vcf_header)
